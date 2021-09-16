1e323e24d4f701eeaf836922f61f8f8e16729999
shiya <shiya@300264.cn>
2021-09-16 17:31:50 +0800

    获取存储空间~

--- demoForWpfCore/MainWindow.xaml.cs
+++ demoForWpfCore/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Aliyun.OSS;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,40 @@ namespace demoForWpfCore
         public MainWindow()
         {
             InitializeComponent();
+            InitConfig();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private static string accessKeyId;
+        private static string accessKeySecret;
+        private static string endpoint;
+
+        private void InitConfig()
+        {
+            accessKeyId = ConfigurationManager.AppSettings["AccessKeyId"] ?? string.Empty;
+            accessKeySecret = ConfigurationManager.AppSettings["AccessKeySecret"] ?? string.Empty;
+            endpoint = ConfigurationManager.AppSettings["Endpoint"] ?? string.Empty;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 由用户指定的OSS访问地址、阿里云颁发的AccessKeyId/AccessKeySecret构造一个新的OssClient实例。
+            var ossClient = new OssClient(endpoint, accessKeyId, accessKeySecret);
+            try
+            {
+                var buckets = ossClient.ListBuckets();
+                ListViewForBucket.ItemsSource = buckets;
+
+                Console.WriteLine("List bucket succeeded");
+                foreach (var bucket in buckets)
+                {
+                    Console.WriteLine("Bucket name：{0}，Location：{1}，Owner：{2}", bucket.Name, bucket.Location, bucket.Owner);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("List bucket failed. {0}", ex.Message);
+            }
         }
     }
 }
